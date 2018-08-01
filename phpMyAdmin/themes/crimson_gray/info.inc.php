d068affe88f4629ab919e83f9955562dc54af144
milob92 <milob92@gmail.com>
2018-07-31 21:52:02 -0500

    er

---
+++ phpMyAdmin/themes/crimson_gray/info.inc.php
@@ -0,0 +1,17 @@
+<?php
+/* vim: set expandtab sw=4 ts=4 sts=4: */
+/**
+ * Theme information
+ * 
+ *
+ * @version $Id: info.inc.php 12496 2009-05-25 15:19:44Z lem9 $
+ * @package phpMyAdmin-theme
+ * @subpackage Crimson-Gray
+ */
+
+/**
+  *
+ */
+$theme_name = 'Crimson-Gray';
+$theme_full_version = '3.0';
+?>
